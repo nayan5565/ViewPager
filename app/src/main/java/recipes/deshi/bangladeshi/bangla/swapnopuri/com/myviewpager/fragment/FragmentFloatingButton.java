9a6026b2164ad14f21690cfa613250597d8335c3
nayan5565 <nayan5565@gmail.com>
2017-07-28 23:08:33 +0600

    Snackbar

--- app/src/main/java/recipes/deshi/bangladeshi/bangla/swapnopuri/com/myviewpager/fragment/FragmentFloatingButton.java
+++ app/src/main/java/recipes/deshi/bangladeshi/bangla/swapnopuri/com/myviewpager/fragment/FragmentFloatingButton.java
@@ -1,13 +1,17 @@
 package recipes.deshi.bangladeshi.bangla.swapnopuri.com.myviewpager.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import recipes.deshi.bangladeshi.bangla.swapnopuri.com.myviewpager.R;
@@ -17,6 +21,8 @@ import recipes.deshi.bangladeshi.bangla.swapnopuri.com.myviewpager.R;
  */
 public class FragmentFloatingButton extends Fragment {
     private FloatingActionButton fab;
+    private Button btnSimpleSnackbar, btnActionCallback, btnCustomView;
+    private CoordinatorLayout coordinatorLayout;
 
     @Nullable
     public static FragmentFloatingButton newInstance() {
@@ -25,16 +31,81 @@ public class FragmentFloatingButton extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_floating, container, false);
-
+        coordinatorLayout = (CoordinatorLayout) view.findViewById(R.id
+                .coordinatorLayout);
         fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        btnSimpleSnackbar = (Button) view.findViewById(R.id.btnSimpleSnackbar);
+        btnActionCallback = (Button) view.findViewById(R.id.btnActionCallback);
+        btnCustomView = (Button) view.findViewById(R.id.btnCustomSnackbar);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(view, "Message is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        }).show();
 //                Toast.makeText(getContext(), "floating button click", Toast.LENGTH_SHORT).show();
             }
         });
+        snc();
         return view;
+    }
+
+    private void snc() {
+
+
+        btnSimpleSnackbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Welcome to AndroidHive", Snackbar.LENGTH_LONG);
+
+                snackbar.show();
+            }
+        });
+
+        btnActionCallback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Message is deleted", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(coordinatorLayout, "Message is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
+            }
+        });
+
+        btnCustomView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                        .setAction("RETRY", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                            }
+                        });
+
+                // Changing message text color
+                snackbar.setActionTextColor(Color.RED);
+
+                // Changing action button text color
+                View sbView = snackbar.getView();
+                TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(Color.YELLOW);
+
+                snackbar.show();
+            }
+        });
     }
 }
